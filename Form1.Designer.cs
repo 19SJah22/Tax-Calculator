731bdd0b41d2e312482cb8d2dae047959b575e65
Sjanrie S Hendrikz <108018913+19SJah22@users.noreply.github.com>
2023-11-28 15:57:47 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace PRG521_FA3_TaxCalculator
+{
+    partial class TaxCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SamountLabel = new System.Windows.Forms.Label();
+            this.StaxLabel = new System.Windows.Forms.Label();
+            this.UserTexBox = new System.Windows.Forms.TextBox();
+            this.PercNum = new System.Windows.Forms.NumericUpDown();
+            this.SubmitButton = new System.Windows.Forms.Button();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.PercNum)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SamountLabel
+            // 
+            this.SamountLabel.AutoSize = true;
+            this.SamountLabel.Location = new System.Drawing.Point(152, 112);
+            this.SamountLabel.Name = "SamountLabel";
+            this.SamountLabel.Size = new System.Drawing.Size(98, 16);
+            this.SamountLabel.TabIndex = 6;
+            this.SamountLabel.Text = "Sales amount : ";
+            this.SamountLabel.Click += new System.EventHandler(this.SamountLabel_Click);
+            // 
+            // StaxLabel
+            // 
+            this.StaxLabel.AutoSize = true;
+            this.StaxLabel.Location = new System.Drawing.Point(173, 171);
+            this.StaxLabel.Name = "StaxLabel";
+            this.StaxLabel.Size = new System.Drawing.Size(77, 16);
+            this.StaxLabel.TabIndex = 7;
+            this.StaxLabel.Text = "Sales Tax : ";
+            this.StaxLabel.Click += new System.EventHandler(this.StaxLabel_Click);
+            // 
+            // UserTexBox
+            // 
+            this.UserTexBox.Location = new System.Drawing.Point(308, 106);
+            this.UserTexBox.Name = "UserTexBox";
+            this.UserTexBox.Size = new System.Drawing.Size(182, 22);
+            this.UserTexBox.TabIndex = 8;
+            this.UserTexBox.TextChanged += new System.EventHandler(this.UserTexBox_TextChanged);
+            // 
+            // PercNum
+            // 
+            this.PercNum.Location = new System.Drawing.Point(308, 165);
+            this.PercNum.Name = "PercNum";
+            this.PercNum.Size = new System.Drawing.Size(138, 22);
+            this.PercNum.TabIndex = 9;
+            this.PercNum.ValueChanged += new System.EventHandler(this.PercNum_ValueChanged);
+            // 
+            // SubmitButton
+            // 
+            this.SubmitButton.Location = new System.Drawing.Point(254, 231);
+            this.SubmitButton.Name = "SubmitButton";
+            this.SubmitButton.Size = new System.Drawing.Size(75, 28);
+            this.SubmitButton.TabIndex = 10;
+            this.SubmitButton.Text = "Submit";
+            this.SubmitButton.UseVisualStyleBackColor = true;
+            this.SubmitButton.Click += new System.EventHandler(this.SubmitButton_Click);
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(347, 231);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(75, 29);
+            this.ClearButton.TabIndex = 11;
+            this.ClearButton.Text = "Clear";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(442, 232);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(75, 28);
+            this.ExitButton.TabIndex = 12;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // TaxCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.SubmitButton);
+            this.Controls.Add(this.PercNum);
+            this.Controls.Add(this.UserTexBox);
+            this.Controls.Add(this.StaxLabel);
+            this.Controls.Add(this.SamountLabel);
+            this.Name = "TaxCalculator";
+            this.Text = "Tax Calculator";
+            ((System.ComponentModel.ISupportInitialize)(this.PercNum)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label SamountLabel;
+        private System.Windows.Forms.Label StaxLabel;
+        private System.Windows.Forms.TextBox UserTexBox;
+        private System.Windows.Forms.NumericUpDown PercNum;
+        private System.Windows.Forms.Button SubmitButton;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.Button ExitButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace PRG521_FA3_TaxCalculator
+{
+    public partial class TaxCalculator : Form
+    {
+        // This is the form
+        public TaxCalculator()
+        {
+            InitializeComponent();
+        }
+
+        // This is the 'Save amount' label
+        private void SamountLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // This is the 'Save Tax' label
+        private void StaxLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // This is the text box where the user will have to input the Rand amount.
+        private void UserTexBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        // This is the numeric box containing the 0 to 25% range.
+        private void PercNum_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        // This is the submit button. When it is clicked the calculations must start and be saved to the txt file.
+        private void SubmitButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (double.TryParse(UserTexBox.Text, out double Sales_Amount_Text))
+                {
+                    double Tax_Amount = (double)PercNum.Value;
+                    if (Tax_Amount >= 0 && Tax_Amount <= 25)
+                    {
+                        double taxAmount = Sales_Amount_Text * (Tax_Amount / 100);
+                        double Amount_Total = Sales_Amount_Text + taxAmount;
+                        string Total_Result = $"Tax on {Sales_Amount_Text:C} at {Tax_Amount}% is {taxAmount:C}. The total is {Amount_Total:C}";
+
+                        // The result must be saved to the TaxCalculation.txt file with the date and time included.
+                        string FileName = "TaxCalculation.txt";
+                        string Formatted_Result = $"{DateTime.Now}: {Total_Result}";
+                        SaveToFile(FileName, Formatted_Result);
+
+                        MessageBox.Show(Total_Result, "Tax Calculation Result: ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        // When user does not enter the correct tax amount they must recieve an error message. 
+                        string error_Message = "The tax rate you have entered is invalid.\n Please enter a tax rate between 0 and 25%";
+                        Exception_To_File_Save(error_Message);
+                        MessageBox.Show(error_Message, "Invalid Tax Rate: ", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                }
+                else
+                {
+                    // When the user does not enter a numeric value they must recieve an error message.
+                    string error_Message = "The value you have entered is not of numeric value. \n Please enter a numeric value for sales amount. ";
+                    Exception_To_File_Save(error_Message);
+                    MessageBox.Show(error_Message, "Invalid Input: ", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+
+            // When the user does not enter the correct input an erroe message must appear.
+            catch (Exception ex)
+            {
+                string error_Message = $"An error occurred: {ex.Message}";
+                Exception_To_File_Save(error_Message);
+                MessageBox.Show(error_Message, "Error: ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Save all the data to the file.
+        private void SaveToFile(string fileName, string content)
+        {
+            using (var writer = new System.IO.StreamWriter(fileName, true))
+            {
+                writer.WriteLine(content);
+            }
+        }
+
+        // When an error appears, save it to Exceptionfile.txt
+        private void Exception_To_File_Save(string errorMessage)
+        {
+            string FileName = "Exceptionfile.txt";
+            string Formatted_Message = $"{DateTime.Now}: {errorMessage}";
+            SaveToFile(FileName, Formatted_Message);
+        }
+
+        // This is the clear button. When the user clicks it all the fields will be reset.
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            UserTexBox.Clear();
+            PercNum.Value = 0;
+        }
+
+        // This is the exit button. When it is clicked the form will be closed.
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit(); 
+        }
+    }
+}
